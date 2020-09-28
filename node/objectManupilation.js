c696b201330b61b70a3d48cba50dbfc9918911df
yusufhayirli <pensarnada@gmail.com>
2020-09-28 20:13:30 +0300

    object manupilation js

--- node/objectManupilation.js
+++ node/objectManupilation.js
@@ -1,9 +1,28 @@
-const obj = {
+// defined a json obj
+let obj = {
     name:'Yusuf',
     age:27,
     isStudent: true
 };
 
-obj.name = 'Osman';
+console.log({...obj, name:'Osman'});
+console.log({...obj, surname:'HAYIRLI'});
+console.log(obj);
+
+console.log('--------------')
+
+// if obj defined with "const" we cannot change it
+// have to use "let"
+obj = {...obj, age:28};
 
 console.log(obj);
+
+const updateObj = (obj,key,value,callback) => {
+    const result = {...obj, [key]: value};
+    callback(result);
+};
+// updates obj's name as Erol and callbacks result
+// as new name (used key,value to do it)
+updateObj(obj,'name','Erol',(result) => {
+    console.log(result);
+})
